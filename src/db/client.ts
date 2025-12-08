38c12d48c3427547c5957affe278912d4b0a3ee5
Jon Deibel <dibesjr@gmail.com>
2025-12-08 14:02:58 -0500

    feat: defaulted db url for easier boostrapping

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/libsql";
 
-const db = drizzle(process.env.DB_FILE_NAME!);
+const db = drizzle(process.env.DB_FILE_NAME ?? "file:local.db");
 
 export { db };
